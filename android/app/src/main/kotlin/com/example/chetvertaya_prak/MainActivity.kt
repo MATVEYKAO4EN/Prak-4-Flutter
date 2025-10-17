a95684d4f9bf5564ae158cff85d50a99cdeda512
MATVEYKAO4EN <matveykao4en@gmail.com>
2025-10-17 14:30:04 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/chetvertaya_prak/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.chetvertaya_prak
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'screens/MainScreen.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Практическая работа №4',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MainScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
